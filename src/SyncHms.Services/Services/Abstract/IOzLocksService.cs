d043e551e4bb12dabddb9b8e11ed4e510d3aa24b
ivankopanyov <i.kopanyov@gmail.com>
2024-10-18 09:08:54 +0300

    Created OzLocksService

---
+++ src/SyncHms.Services/Services/Abstract/IOzLocksService.cs
@@ -0,0 +1,9 @@
+﻿namespace SyncHms.Services;
+
+/// <summary>
+/// Интерфейс, определяющий методы сервиса интеграции с базой данных <c>OzLocks</c><br/>
+/// Расширяет методы интерфейса <see cref="IService{TOptions,TEnvironment}"/>
+/// </summary>
+public interface IOzLocksService : IService<OzLocksOptions, ApplicationEnvironment>
+{
+}

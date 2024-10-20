b22b9979811004a8a6f028bfc9fc038e808f7c02
ivankopanyov <i.kopanyov@gmail.com>
2024-10-20 12:39:42 +0300

    Added schedule migration

---
+++ src/SyncHms.Domain.EntityFramework.Sqlite/Migrations/20241020093908_AddedSchedule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncHms.Domain.EntityFramework.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    IntervalSeconds = table.Column<int>(type: "INTEGER", nullable: false),
+                    Last = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
